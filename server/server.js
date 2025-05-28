975ec1729ce461d9ec889615233d99f13e5f9581
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-05-28 23:10:26 +0800

    v0.2.0 - HTTP Server w/ Request handler express done, tested api route

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import http from 'http'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import app from './app.js'
+
+// Configuration
+dotenv.config();
+colors.enable();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
+
+// Database
+
+// Server
+const server = http.createServer(app);
+
+server.listen(PORT, ()=>{
+    console.log(
+        '\n\n\n\nLINK: '.cyan + ('http://127.0.0.1:' + PORT + '/\n').yellow.italic.underline
+    );
+})
+
+
+export default server;
+
