e02f5c03fe4296eba684a72a38543b77ba8d65de
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-21 23:42:27 +0800

    v0.4.3 - Test React Build in the server View for assurance

--- server/config/app.js
+++ server/config/app.js
@@ -1,7 +1,7 @@
-import express, { urlencoded } from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import express, { urlencoded } from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -9,36 +9,31 @@ import { fileURLToPath } from 'url';
 dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const viewPath = path.join(__dirname, '../View');
 
 // Request Handler
 const app = express();
 
 // Middleware
-app.use(urlencoded({extended: true}));
+app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../View')));
+app.use(express.static(viewPath));
 
-app.use(cors({
-    origin: "*",
-    allowedHeaders: ['POST', 'GET'],
-    credentials: true,
-}));
-
+app.use(
+    cors({
+        origin: '*',
+        allowedHeaders: ['POST', 'GET'],
+        credentials: true,
+    })
+);
 
 // API Route
-import index from '../Router/index.js'
+import index from '../Router/index.js';
 app.use('/api', index);
 
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../View/index.html'), (err) => {
-        if (err) {
-            console.error("Error sending file:", err);
-            res.status(500).send("Internal Server Error");
-        } else {
-            console.log("File sent successfully");
-        }
-    });
+app.use((req, res) => {
+    res.sendFile(viewPath);
 });
 
 export default app;
