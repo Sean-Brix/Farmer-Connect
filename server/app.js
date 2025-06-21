6014ad870aff4ae8b1a6492817aa1d495752c744
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-21 22:53:35 +0800

    v0.4.1 - JWT token utils use for middleware

--- server/app.js
+++ server/app.js
@@ -1,21 +1,14 @@
 import express, { urlencoded } from 'express'
-import path from 'path'
-import {fileURLToPath} from 'url'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
 // Configuration
 dotenv.config();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-// Request Handler
 const app = express();
 
 // Middleware
-app.use(urlencoded({extended: false}));
+app.use(urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -23,7 +16,6 @@ app.use(cors({
     origin: "*",
     allowedHeaders: ['POST', 'GET'],
     credentials: true,
-    
 }));
 
 // Route
