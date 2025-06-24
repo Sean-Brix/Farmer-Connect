987a899e47264ed50172af7ec20cfd375fe79876
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-24 18:32:27 +0800

    v0.8.1 - first db prisma migration, initialized database

--- server/prisma/generated/wasm.js
+++ server/prisma/generated/wasm.js
@@ -178,15 +178,6 @@ exports.Prisma.Inventory_categoriesScalarFieldEnum = {
   updatedAt: 'updatedAt'
 };
 
-exports.Prisma.Borrowed_itemsScalarFieldEnum = {
-  id: 'id',
-  itemId: 'itemId',
-  borrowedAt: 'borrowedAt',
-  returnedAt: 'returnedAt',
-  borrowerId: 'borrowerId',
-  notes: 'notes'
-};
-
 exports.Prisma.SeminarsScalarFieldEnum = {
   id: 'id',
   title: 'title',
@@ -266,11 +257,6 @@ exports.Prisma.inventory_categoriesOrderByRelevanceFieldEnum = {
   description: 'description'
 };
 
-exports.Prisma.borrowed_itemsOrderByRelevanceFieldEnum = {
-  borrowerId: 'borrowerId',
-  notes: 'notes'
-};
-
 exports.Prisma.seminarsOrderByRelevanceFieldEnum = {
   id: 'id',
   title: 'title',
@@ -337,7 +323,6 @@ exports.Prisma.ModelName = {
   accounts_commodities: 'accounts_commodities',
   inventory_items: 'inventory_items',
   inventory_categories: 'inventory_categories',
-  borrowed_items: 'borrowed_items',
   seminars: 'seminars',
   seminar_participants: 'seminar_participants'
 };
