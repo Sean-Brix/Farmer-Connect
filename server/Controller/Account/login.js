9915834a174398bf03af3dddba2dbd4574655f2d
Kc Sean Brix Calinao <kcseancalinao@gmail.com>
2025-06-26 06:23:30 +0800

    v0.10.2 - Login controller function for authentication

---
+++ server/Controller/Account/login.js
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+async function login(req, res) {
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.accounts.findUnique({
+            where: {
+                username: username,
+            },
+        });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        if (user.password !== password) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        // Here you would typically create a session or token
+        return res.status(200).json({ message: 'Login successful', user });
+    } 
+    
+    catch (error) {
+
+        console.error('Error during login:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+
+    }
+}
+
+export default login;
--- server/Router/Auth/auth.js
+++ server/Router/Auth/auth.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import login from '../../Controller/Account/login.js';
 
 // Route: ('/auth')
 const router = express.Router();
+
+router.use('/login', login);
 
 export default router;
